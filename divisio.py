7d30905add0bd0df2a9f70a38f186cb38de95b9f
Tiago <tquispe@instituticaria.cat>
2024-10-16 13:52:08 +0200

    primer enviament

---
+++ divisio.py
@@ -0,0 +1,8 @@
+dividend =int(input("introduïu el dividend:"))
+divisor =int(input("introduïu el divisor:"))
+división= dividend/divisor
+quocient= dividend//divisor
+residu= dividend%divisor
+print(f"Divisió: {dividend}/{divisor}")
+print("Quocient:", quocient)
+print("Residu:", residu)
